d66398b4f509e92d5a8f6235caacd9392c57a024
Sam Carré <29132017+Sammyjo20@users.noreply.github.com>
2022-12-23 13:42:31 +0000

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,9 +3,12 @@
 namespace Plannr\Laravel\FastRefreshDatabase\Tests;
 
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use Plannr\Laravel\FastRefreshDatabase\Traits\FastRefreshDatabase;
 
 class TestCase extends BaseTestCase
 {
+    use FastRefreshDatabase;
+
     /**
      * Define database migrations.
      *
